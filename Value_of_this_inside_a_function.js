2fc9de5c5abe7d247d3a70977e000cf0125d1594
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-11-19 11:31:03 +0530

    feat: implement function demonstrating 'this' behavior in strict and non-strict modes
    - Added a function that logs the value of 'this'.
    - Demonstrates 'this' pointing to the global object in non-strict mode.
    - Shows 'this' being undefined in strict mode.
    - Handles 'this' referencing an object when invoked as a method of that object.

--- Value_of_this_inside_a_function.js
+++ Value_of_this_inside_a_function.js
@@ -2,8 +2,12 @@
 function printThis(){
     console.log(this)
 }
-printThis();
+
 
 /* if we use this key  word inside a function in Node.js environment it's value will be global object
 but in browser environment it's value will be the window object when we run the code in non-strict mode
 but if we run the code in strict-mode it's value will be un-defined*/
+
+//But if we call the function in refernece to someObject then the value of this keyword wil be equal 
+//to that object For Eg:
+window.printThis(); //Here the value of this keyword will be equal to window object in browsers
