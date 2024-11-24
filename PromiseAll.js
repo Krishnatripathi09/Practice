7989fc85ed90870afd54f698167a618493c08586
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-11-24 14:19:32 +0530

    Updated to handle error with catch block

--- PromiseAll.js
+++ PromiseAll.js
@@ -2,15 +2,24 @@ const p1 = new Promise((resolve,reject)=>{
     setTimeout(()=>resolve("P1 Successful"),3000)
 })
 const p2 = new Promise((resolve,reject)=>{
-    setTimeout(()=>resolve("P2 Successful"),1000)
+    setTimeout(()=>reject("P2 Fail"),1000)
 })
 const p3 = new Promise((resolve,reject)=>{
     setTimeout(()=>resolve("P3 Successful"),2000)
 })
 Promise.all([p1,p2,p3])
-.then(res=>{console.log(res)}); 
+.then(res=>{console.log(res);
+})
+.catch((err)=>{
+console.error(err)
+});
 /*  Promise.all is a promise Api which takes an array of promises and returns the result if all 
-the promises are fulfilled Even if a single promise is rejected is will quickly log the error 
+the promises are fulfilled Even if a single promise is rejected it will quickly log the error 
 to the console for that failed promise. In this case it returns p1,p2,p3 successfull after 3 seconds 
 as the setTimeOut value we have set is 3 seconds for highest so it waits for all the promises to get 
 fulfilled and then returns the result*/
+//In Above Example we have set promise p2 to fail after 1 sec so it will fail after 1 sec and 
+//promise.all will return an error and catch will catch that error and log it to the console.
+
+
+
