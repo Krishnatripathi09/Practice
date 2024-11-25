521e66d265d6f11501919d738b7f29c5a3607543
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-11-25 15:58:45 +0530

    feat:Added code demonstrating Immediately Invoked Function Expressions (IIFE) to showcase function scope and self-execution in JavaScript.

---
+++ IIFE.js
@@ -0,0 +1,8 @@
+(function print(){
+    console.log("Hello world")
+}
+)();
+
+/* IIFE (Immediately invoked function expression  is executed immediately after it's creation
+variable defined IIFE ar enot accessible from outside it's scope which help's to avoid conflict with
+other variable's or functions.IIFE keeps variables or functions private in it.*/  
